82f55f22efb2bf878a2012a47edb1cdf42e7af54
rszumlas <r_szumlas@o2.pl>
2022-10-30 21:41:46 +0100

    feat: add OrderTypeApplication.java fix: rename spring.datasource.url in all application.yaml's fix: change jpa.hibernate.ddl-auto to update for all application.yaml's

---
+++ order-type/src/main/java/com/rszumlas/ethanol/OrderTypeApplication.java
@@ -0,0 +1,13 @@
+package com.rszumlas.ethanol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderTypeApplication.class, args);
+    }
+
+}
