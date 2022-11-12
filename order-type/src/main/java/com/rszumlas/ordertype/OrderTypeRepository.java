c71f31d179f8d43c161f40288bf2600158aecedb
rszumlas <r_szumlas@o2.pl>
2022-11-12 22:12:15 +0100

    refactor: remove order type module cuz not needed

--- order-type/src/main/java/com/rszumlas/ordertype/OrderTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rszumlas.ordertype;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderTypeRepository extends JpaRepository<OrderType, Long> {
-}
