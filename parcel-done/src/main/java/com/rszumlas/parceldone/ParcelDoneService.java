fe58bd92e854ddfb2b2eb98c24f5b456495d1d87
rszumlas <r_szumlas@o2.pl>
2022-12-13 19:02:35 +0100

    fix: calculateEarnedEth working properly

--- parcel-done/src/main/java/com/rszumlas/parceldone/ParcelDoneService.java
+++ parcel-done/src/main/java/com/rszumlas/parceldone/ParcelDoneService.java
@@ -1,5 +1,6 @@
 package com.rszumlas.parceldone;
 
+import com.rszumlas.amqp.RabbitMQMessageProducer;
 import com.rszumlas.clients.account.AccountClient;
 import com.rszumlas.clients.parceldone.ParcelDoneRequest;
 import com.rszumlas.clients.shelf.ShelfClient;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -16,9 +18,9 @@ public class ParcelDoneService {
     private final ParcelDoneRepository parcelDoneRepository;
     private final ShelfClient shelfClient;
     private final AccountClient accountClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
     public void insertParcelDone(ParcelDoneRequest parcelDoneRequest) {
-
         parcelDoneRepository.insertParcelDone(parcelDoneRequest);
         shelfClient.updateCratesAmount(parcelDoneRequest.parcel_id());
         accountClient.updateEthTotal(parcelDoneRequest.account_id(), calculateEarnedEth(parcelDoneRequest.delivery_time_seconds()));
@@ -27,9 +29,9 @@ public class ParcelDoneService {
     private Double calculateEarnedEth(Integer delivery_time_seconds) {
         float plnPerHour = 19.7f;
         int ethPriceInUsd = 1300;
-        double usdPerSecond = 19.7/4.5 / 3600;
+        double usdPerSecond = plnPerHour/4.5 / 3600;
         double ethPerSecond = usdPerSecond/ethPriceInUsd;
-        BigDecimal earnedEth = BigDecimal.valueOf(usdPerSecond * delivery_time_seconds).setScale(4, RoundingMode.DOWN);
+        BigDecimal earnedEth = BigDecimal.valueOf(ethPerSecond * delivery_time_seconds).setScale(4, RoundingMode.DOWN);
         return earnedEth.doubleValue();
     }
 
